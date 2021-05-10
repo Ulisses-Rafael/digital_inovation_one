aad612cac16183e57534cdc72117c38982c5a50a
Ulisses-Rafael <ulissesfavaris@yahoo.com.br>
2021-05-10 14:09:43 -0300

    introdução javascript
    Comandos básicos js, e introdução a linguagem.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "Rafael Favaris";
+var idade = "37";
+var frase = "O Japão é o melhor time do mundo"
+// alert(nome + " tem "+idade);
+console.log(nome);
+console.log(idade);
+//console.log(frase.replace("japão","Brasil"));
+console.log(frase.toLocaleUpperCase());
